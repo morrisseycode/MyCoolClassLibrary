88fd30949f89f736d3e313244deb833fa6334296
morsesolutions <morsesolutions@outlook.com>
2021-11-25 16:13:02 -0500

    Add project files.

---
+++ MyCoolClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCoolClassLibrary
+{
+    public class Class1
+    {
+        public int AddInts(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
